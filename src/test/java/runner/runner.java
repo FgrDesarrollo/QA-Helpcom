9d405eabcc4fd692563d22a22d0e7720353278dc
leonardo <leonardo.fgr@gmail.com>
2021-08-27 17:11:22 -0400

    commit iinicial

---
+++ src/test/java/runner/runner.java
@@ -0,0 +1,15 @@
+package runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/test/resources/features", // se usa para decirle a cucmber dodne estan los features
+    glue = "steps"// glue se usa para decirle a cucumbre donde tenemos los steps
+)
+public class runner {
+
+}
---
+++ src/test/java/steps/GoogleSteps.java
@@ -0,0 +1,27 @@
+package steps;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class GoogleSteps {
+    /*En el comentario del Given , se recomienda usar los simbolos ^ $ al principio y al final respectivamente para decirle a cucumber que esto es un test
+    / tambien sirve para evitar que cucumber se pierda con definicion de clases que comiencen con nombres similares*/
+    @Given("^navego en google$")
+    public void navigateToGoogle(){
+
+    } 
+    @When("^busco algo$")
+    public void buscoAlgo(){
+
+    } 
+    @And("^presiono el boton buscar$")
+    public void presionoBoton(){
+
+    } 
+    @Then("^Obtengo algo$")
+    public void obtengoResultado(){
+
+    } 
+}
