8edca26a61efe3624c15ff965316b9bb7253e0c4
Leonardo Figueroa Sandoval <leonardo@MacBook-Pro-de-Leonardo.local>
2021-08-30 13:18:12 -0400

    primer commit desde maquina nueva, todo funcionando

--- build/resources/test/features/google.feature
+++
@@ -1,8 +0,0 @@
-Feature: Probar la busqueda en google
-
-Scenario: Un usuario busca algo en google
-    Given   navego en google
-    When    busco algo
-    And     presiono el boton buscar
-    Then    Obtengo algo
-
--- src/test/java/steps/GoogleSteps.java
+++ src/test/java/steps/GoogleSteps.java
@@ -4,12 +4,20 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import pages.GooglePage;
 
 public class GoogleSteps {
+
     /*En el comentario del Given , se recomienda usar los simbolos ^ $ al principio y al final respectivamente para decirle a cucumber que esto es un test
     / tambien sirve para evitar que cucumber se pierda con definicion de clases que comiencen con nombres similares*/
+    
+    GooglePage google = new GooglePage();
+
+    
+    
     @Given("^navego en google$")
     public void navigateToGoogle(){
+        google.navigateToGoogle();
 
     } 
     @When("^busco algo$")
